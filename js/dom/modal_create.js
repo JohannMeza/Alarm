7d14daea5114bab9d72102ba4bec1b8a69df3b73
JohannMeza <81168147+JohannMeza@users.noreply.github.com>
2021-09-20 00:26:40 -0500

    Update modal_create.js

--- js/dom/modal_create.js
+++ js/dom/modal_create.js
@@ -4,7 +4,7 @@ const $modal = d.querySelector(".modal")
 export default function modalNew () {
   d.addEventListener("click", e => {
     if (e.target.matches(".contain-edit-btn, .contain-edit-btn *")) {
-      for (let el = 0; el <= 24; el++) {
+      for (let el = 1; el <= 24; el++) {
         let $option = d.createElement("option");
         $option.textContent = el.toString().length === 1  ? `0${el.toString()} horas` : `${el} horas`;
         $modal.querySelector(".form__select-hour").appendChild($option)
@@ -16,7 +16,7 @@ export default function modalNew () {
         $modal.querySelector(".form__select-minutes").appendChild($option)
       }
       
-      for (let el = 0; el < 30; el ++) {
+      for (let el = 5; el < 30; el ++) {
         let $option = d.createElement("option");
         $option.textContent = el.toString().length === 1  ? `0${el.toString()} minutos` : `${el} minutos`;
         $modal.querySelector(".form__select-descanso").appendChild($option)
